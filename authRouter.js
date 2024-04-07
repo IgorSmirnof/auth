6e3948e0a2de28be81bb17152acc24618d4fd27f
IGo <igor.smirna@gmail.com>
2024-04-07 22:14:45 +0300

    feat cng contr for test

--- authRouter.js
+++ authRouter.js
@@ -6,13 +6,13 @@ const authMiddleware = require('./middlewaree/authMiddleware')
 const roleMiddleware = require('./middlewaree/roleMiddleware')
 
 router.post('/signup', [
-    check('username', "Имя пользователя не может быть пустым").notEmpty(),
-    check('password', "Пароль должен быть больше 4 и меньше 10 символов").isLength({min:4, max:10})
+    check('email', "Field не может быть пустым").notEmpty(),
+    check('password', "Пароль должен быть меньше 4 и больше 10 символов").isLength({min:4, max:10})
 ], controller.registration)
 router.post('/signin', controller.login)
 router.get('/users/me', controller.getUsers)
-router.post('/', controller.any)
+router.get('/users', roleMiddleware(["ADMIN"]), controller.getUsers)
+router.post('/*', controller.any)
 
-// router.get('/users', roleMiddleware(["ADMIN"]), controller.getUsers)
 
 module.exports = router
